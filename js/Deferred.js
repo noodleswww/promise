b04a83d05309e7aa0edb2d9dc8c1024d9639e188
vendetta14 <460873206@qq.com>
2015-08-16 20:22:21 +0800

    no message

--- js/Deferred.js
+++ js/Deferred.js
@@ -1,3 +1,49 @@
-/**
- * Created by noodles on 8/16 0016.
- */
++(function (name, func) {
+    if (typeof define === 'function') {
+        define(name, define);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = func();
+    } else {
+        window[name] = func();
+    }
+})('Deferred', function () {
+    var Deferred = function () {
+        this.promise = new Promise();
+    };
+    Deferred.prototype.resolve = function (arr) {
+        var promise = this.promise;
+        var _ref;
+        while ((_ref = promise.queue.shift())) {
+            if (_ref && _ref.fulfilled) {
+                var ret = _ref.fulfilled(arr);
+                if (ret && ret.isPromise) {
+                    ret.queue = promise.queue;
+                    this.promise = ret;
+                    return;
+                }
+            }
+        }
+    };
+    Deferred.prototype.reject = function (err) {
+        var promise = this.promise;
+        var _ref;
+        while ((_ref = promise.queue.shift())) {
+            if (_ref && _ref.error) {
+                var ret = _ref.error(err);
+                if (ret && ret.isPromise) {
+                    ret.queue = promise.queue;
+                    this.promise = ret;
+                    return;
+                }
+            }
+        }
+    };
+    Deferred.prototype.callback = function () {
+        var _this = this;
+        return function (err) {
+            if (err) return _this.reject(err);
+            _this.resolve(Array.prototype.slice.call(arguments, 1));
+        }
+    };
+    return Deferred;
+});
--- js/Promise.js
+++ js/Promise.js
@@ -1,3 +1,26 @@
-/**
- * Created by noodles on 8/16 0016.
- */
++(function (name, func) {
+    if (typeof define === 'function') {
+        define(name, define);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = func();
+    } else {
+        window[name] = func();
+    }
+})('Promise', function () {
+    var Promise = function () {
+        this.queue = [];
+        this.isPromise = true;
+    };
+    Promise.prototype.then = function (fulfilledHandler, errHandler) {
+        var handler = {};
+        if (typeof fulfilledHandler === 'function') {
+            handler.fulfilled = fulfilledHandler;
+        }
+        if (typeof errHandler === 'function') {
+            handler.error = errHandler;
+        }
+        this.queue.push(handler);
+        return this;
+    };
+    return Promise;
+});
